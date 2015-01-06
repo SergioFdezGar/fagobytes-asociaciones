87d692f00c505c7bfcff1502833fb2d19579de52
sergio.fdz.gar@gmail.com <sergio.fdz.gar@gmail.com@ca82dd82-1966-ac62-eb07-644ed2a2b6c8>
2015-01-06 19:33:32 +0000

    Revision de UsuarioDAOTest

--- gest_asobu/src/test/java/g01_03/gest_asobu/dominio/UsuarioDAOTest.java
+++ gest_asobu/src/test/java/g01_03/gest_asobu/dominio/UsuarioDAOTest.java
@@ -13,79 +13,81 @@ public class UsuarioDAOTest extends TestCase{
         	 	 gesper = new UsuarioDAO();
                  prueba=gesper.autenticar(null, null);
                  assertFalse(prueba);
-
- }
+                 
+	 }
  
- public void testAutenticar2() throws Exception{
+	 public void testAutenticar2() throws Exception{
+		 	 UsuarioDAO gesper;
+	         boolean prueba=true;
+	
+	        	 	 gesper = new UsuarioDAO();
+	                 prueba=gesper.autenticar("hola", "123456458");
+	                 assertFalse(prueba);
+	 }
+	 
+	 public void testAutenticar3() throws Exception{
 	 	 UsuarioDAO gesper;
-         boolean prueba=true;
-
-        	 	 gesper = new UsuarioDAO();
-                 prueba=gesper.autenticar("hola", "123456458");
-                 assertFalse(prueba);
- }
- 
- public void testAutenticar3() throws Exception{
- 	 UsuarioDAO gesper;
-     boolean prueba=true;
-
-    	 	 gesper = new UsuarioDAO();
-             prueba=gesper.autenticar("sergio", "juas");
-            //
-             assertTrue(prueba);
-}
- 
- public void testAgregar() throws Exception {
-	 UsuarioDAO gesper;
-	 Usuario user= new Usuario("prueba", "123");
-     boolean prueba=true;
-    	 	 gesper = new UsuarioDAO();
-             prueba=gesper.agregar(user);
-             //
-             assertTrue(prueba);
-
-}
- 
- public void testEliminar1() throws Exception {
-	 UsuarioDAO gesper;
-	 Usuario user= new Usuario("prueba", "123");
-     boolean prueba=true;
-    	 	 gesper = new UsuarioDAO();
-             prueba=gesper.eliminar(user);
-             //
-             assertFalse(prueba);
-
-}
- 
- public void testEliminar2() throws Exception {
-	 UsuarioDAO gesper;
-	 Usuario user= new Usuario("prueba2", "456");
-     boolean prueba=true;
-    	 	 gesper = new UsuarioDAO();
-             prueba=gesper.eliminar(user);
-             //
-             assertTrue(prueba);
-
-}
- 
- public void testModificar() throws Exception {
-	 UsuarioDAO gesper;
-	 Usuario user= new Usuario("prueba", "123");
-	 Usuario user2= new Usuario("prueba2", "456");
-     boolean prueba=true;
-    	 	 gesper = new UsuarioDAO();
-             prueba=gesper.modificar(user,user2);
-             //
-             assertTrue(prueba);
-
-}
- public void testModificar2() throws Exception {
-	 UsuarioDAO gesper;
-	 Usuario user= new Usuario("prueba", "123");
-     boolean prueba=true;
-    	 	 gesper = new UsuarioDAO();
-             prueba=gesper.modificar(user);
-             assertFalse(prueba);
-
-}
+	     boolean prueba=true;
+	
+	    	 	 gesper = new UsuarioDAO();
+	             prueba=gesper.autenticar("sergio", "juas");
+	            //
+	             assertTrue(prueba);
+	}
+	 
+	 public void testAgregar() throws Exception {
+		 UsuarioDAO gesper;
+		 Usuario user= new Usuario("prueba", "123");
+	     boolean prueba=true;
+	    	 	 gesper = new UsuarioDAO();
+	             prueba=gesper.agregar(user);
+	             //
+	             assertTrue(prueba);
+	
+	}
+	 
+	 public void testEliminar1() throws Exception {
+		 UsuarioDAO gesper;
+		 Usuario user= new Usuario("prueba", "123");
+	     boolean prueba=true;
+	    	 	 gesper = new UsuarioDAO();
+	             prueba=gesper.eliminar(user);
+	             //
+	             assertFalse(prueba);
+	
+	}
+	 
+	 public void testModificar() throws Exception {
+		 UsuarioDAO gesper;
+		 Usuario user= new Usuario("prueba", "123");
+		 Usuario user2= new Usuario("prueba2", "456");
+	     boolean prueba=true;
+	    	 	 gesper = new UsuarioDAO();
+	             prueba=gesper.modificar(user,user2);
+	             //
+	             assertTrue(prueba);
+	
+	} 
+	 
+	public void testEliminar2() throws Exception {
+		 UsuarioDAO gesper;
+		 Usuario user= new Usuario("prueba2", "456");
+	     boolean prueba=true;
+	    	 	 gesper = new UsuarioDAO();
+	             prueba=gesper.eliminar(user);
+	             //
+	             assertTrue(prueba);
+	
+	}
+	 
+	 
+	 public void testModificar2() throws Exception {
+		 UsuarioDAO gesper;
+		 Usuario user= new Usuario("prueba", "123");
+	     boolean prueba=true;
+	    	 	 gesper = new UsuarioDAO();
+	             prueba=gesper.modificar(user);
+	             assertFalse(prueba);
+	
+	}
 }
